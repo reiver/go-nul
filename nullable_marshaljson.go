168345c9e78dc7ebb97f2cb2f54b8a87b6beb49f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-24 09:40:03 +0900

    initial commits

---
+++ nullable_marshaljson.go
@@ -0,0 +1,29 @@
+package nul
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+var _ json.Marshaler = Nothing[bool]()
+var _ json.Marshaler = Nothing[string]()
+
+// MarshalJSON makes it so json.Marshaler is implemented.
+func (receiver Nullable[T]) MarshalJSON() ([]byte, error) {
+	switch interface{}(receiver.value).(type) {
+	case bool, string:
+		// these are OK.
+	default:
+		return nil, fmt.Errorf("cannot marshal something of type %T into JSON because parameterized type is ‘%T’ rather than ‘bool’ or ‘string’", receiver, receiver.value)
+	}
+
+	if receiver.isnothing() {
+		return nil, fmt.Errorf("cannot marshal opt.Nothing[%T]() into JSON", receiver.value)
+	}
+
+	if receiver.isnull {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(receiver.value)
+}
